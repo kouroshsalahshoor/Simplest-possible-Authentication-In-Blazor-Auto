9d36e59e12b5f77447149d9f4840a957613f726f
kourosh <kourosh23@hotmail.com>
2024-04-07 11:18:06 +0200

    Add project files.

---
+++ BlazorApp1/BlazorApp1/Auth/DummyAuthenticationStateProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorApp1.Auth
+{
+    public class DummyAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            //await Task.Delay(3000);
+
+            var authorized = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "User Name"),
+                new Claim(ClaimTypes.Email, "user@x.x"),
+                new Claim(ClaimTypes.NameIdentifier, "User Id"),
+                new Claim(ClaimTypes.Role, "User"),
+                new Claim("key1", "value1"),
+            }, "xxx");
+            //var authorized = new ClaimsIdentity("xxx");
+
+            var anonymous = new ClaimsIdentity();
+            
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(authorized)));
+            //return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonymous)));
+        }
+    }
+}
